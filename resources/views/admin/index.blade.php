b0ec033324325984d0bfde085419101ddc397648
MALE <mariaalejandraquintanilla6@gmail.com>
2022-04-14 18:57:05 -0400

    actualizado

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,20 @@
+@extends('adminlte::page')
+
+@section('title', 'PiedPipper')
+
+@section('content_header')
+    <h1>Pied Pipper</h1>
+@stop
+
+@section('content')
+    <p>Bienvenido! ¿Estas listo para comenzar a administrar?</p>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
+
